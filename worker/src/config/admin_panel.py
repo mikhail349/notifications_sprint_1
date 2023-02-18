b0d6ace81b231280031fe97e081833219e52a3b5
mikhail349 <mikhail349349@gmail.com>
2023-02-18 11:14:47 +0300

    Revert "Update admin_panel.py"
    This reverts commit b4932c9f68864e6a6804c7c336fc1ec8e0d55665.

--- worker/src/config/admin_panel.py
+++ worker/src/config/admin_panel.py
@@ -1,5 +1,5 @@
 """Настройки для работы с сервисом admin panel."""
-from pydantic import Field
+from pydantic import Field, Required
 
 from src.config.base import BaseConfig
 
@@ -7,7 +7,7 @@ from src.config.base import BaseConfig
 class AdminPanelSettings(BaseConfig):
     """Настройки Admin panel."""
 
-    api_url: str = Field('127.0.0.1:8080/api/v1', env='ADMIN_PANEL_API')
+    api_url: str = Field(Required, env='ADMIN_PANEL_API')
 
 
 admin_panel_config = AdminPanelSettings()
